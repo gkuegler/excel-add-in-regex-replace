ea43da2439ed0129f5f0b27df6f87917a9e91772
George Kuegler <georgekuegler@gmail.com>
2024-01-04 17:51:52 -0500

    implement controls

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -1,68 +1,142 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the
- * MIT license. See LICENSE in the project root for license information.
+/**
+ * API REFERENCE
+ * https://learn.microsoft.com/en-us/javascript/api/excel?view=excel-js-preview
  */
 
-/* global console, document, Excel, Office */
+// TODO: enable every reporting dialogbox
+// TODO: enable match options and modes
+// TODO: fixed display alignment issues
+// TODO: implement custom undo
+
+// All the controls except the 'match mode' have been implemented.
+
+function replaceTableData(table, data) {
+  // Testing data.
+  // const data = [
+  // ["A1", "existing", "replacement"],
+  // ["B1", "existing", "replacement"],
+  // ];
+
+  // If there are no data to add. Clear the table and return.
+  if (data.length < 1) {
+    table.tBodies[0].remove(); // 1 DOM reflow 1 repaint
+    return;
+  }
+  const rows = data.length;
+  const columns = data[0].length;
+  const tB = document.createElement("tbody");
+  const frag = document.createDocumentFragment();
+
+  for (let r = 0; r < rows; r++) {
+    let row = tB.insertRow();
+    for (let c = 0; c < columns; c++) {
+      row.insertCell().textContent = data[r][c];
+    }
+  }
+  // Remove the existing table body.
+  if (table.tBodies.length > 0) {
+    table.tBodies[0].remove(); // 1 DOM reflow 1 repaint
+  }
+
+  // Add the table body.
+  if (data.length > 0) {
+    frag.append(tB);
+    table.append(frag); // 1 DOM reflow 1 repaint
+  }
+}
 
 Office.onReady((info) => {
   if (info.host === Office.HostType.Excel) {
-    // Assign event handlers and other initialization logic.
-    // document.getElementById("create-table").onclick = () => tryCatch(createTable);
+    // Present to the user that an error method.
     document.getElementById("sideload-msg").style.display = "none";
+
+    // Assign event handlers
+    document.getElementById("search").onclick = () => tryCatch(onSearch);
+    document.getElementById("replace").onclick = () => tryCatch(onReplace);
+
     document.getElementById("app-body").style.display = "flex";
   }
 });
 
-export async function replace_selection() {
-  try {
-    await Excel.run(async(context) => {
-      /**
-       * Insert your Excel code here
-       */
-      const range = context.workbook.getSelectedRange();
-
-      // Read the range address
-      range.load("address");
-
-      // Update the fill color
-      range.format.fill.color = "yellow";
-
-      await context.sync();
-    });
-  } catch (error) {
-    console.error(error);
-  }
+async function onSearch() {
+  await searchOrReplace(true);
 }
 
-async function createTable() {
-  await Excel.run(async(context) => {
-    // TODO1: Queue table creation logic here.
-    const currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
-    const expensesTable =
-        currentWorksheet.tables.add("A1:D1", true /*hasHeaders*/);
-    expensesTable.name = "ExpensesTable";
+async function onReplace() {
+  await searchOrReplace(false);
+}
 
-    // TODO2: Queue commands to populate the table with data.
-    expensesTable.getHeaderRowRange().values =
-        [[ "Date", "Merchant", "Category", "Amount" ]];
+async function searchOrReplace(searchOnly) {
+  await Excel.run(async (context) => {
+    var searchPattern = document.getElementById("search_ptrn").value;
+    var replacePattern = document.getElementById("replace_ptrn").value;
+    var useRegex = document.getElementById("useRegex").checked;
+    var caseInsensitive = document.getElementById("caseInsensitive").checked;
+    var useActiveSelection = document.getElementById("useActiveSelection").checked;
 
-    expensesTable.rows.add(null /*add at the end*/, [
-      [ "1/1/2017", "The Phone Company", "Communications", "120" ],
-      [ "1/2/2017", "Northwind Electric Cars", "Transportation", "142.33" ],
-      [ "1/5/2017", "Best For You Organics Company", "Groceries", "27.9" ],
-      [ "1/10/2017", "Coho Vineyard", "Restaurant", "33" ],
-      [ "1/11/2017", "Bellows College", "Education", "350.1" ],
-      [ "1/15/2017", "Trey Research", "Other", "135" ],
-      [ "1/15/2017", "Best For You Organics Company", "Groceries", "97.88" ]
-    ]);
+    // Find the match mode radio box election.
+    var radios = document.getElementsByName("matchMode");
+    var matchMode;
 
-    // TODO3: Queue commands to format the table.
-    expensesTable.columns.getItemAt(3).getRange().numberFormat =
-        [['\u20AC#,##0.00']];
-    expensesTable.getRange().format.autofitColumns();
-    expensesTable.getRange().format.autofitRows();
-    console.error("An error occured");
+    for (var i = 0, length = radios.length; i < length; i++) {
+      if (radios[i].checked) {
+        // only one radio can be logically checked, don't check the rest
+        matchMode = radios[i].value;
+        break;
+      }
+    }
+
+    var flags = "";
+
+    if (caseInsensitive) {
+      flags += "i";
+    }
+
+    var regexPatternFull = useRegex ? new RegExp(searchPattern, flags) : searchPattern;
+
+    var range = useActiveSelection
+      ? context.workbook.getSelectedRange()
+      : context.workbook.worksheets.getActiveWorksheet().getUsedRange();
+
+    // Need to load the Excel proxy object property.
+    range.load("address");
+    range.load("rowCount");
+    range.load("columnCount");
+    range.load("values");
+
+    // Need to call the sync after the load function
+    // before I can access the proxy object property.
+    await context.sync();
+
+    // Apparently 'forEach' not a function
+    // range.forEach(row => {})
+
+    var values = range.values;
+    var newHtmlTableRows = [];
+    for (var i = 0; i < range.rowCount; i++) {
+      for (var j = 0; j < range.columnCount; j++) {
+        const existing = values[i][j];
+        // If cell is string type that is not empty.
+        if ((typeof existing === "string") & (existing != "")) {
+          const replacement = existing.replace(regexPatternFull, replacePattern);
+
+          // Find if the cell's contents would be replaced.
+          if (replacement != existing) {
+            // Add to the output table.
+            newHtmlTableRows.push(["A1", existing, replacement]);
+            values[i][j] = replacement;
+          }
+        }
+      }
+    }
+
+    replaceTableData(document.querySelector("table"), newHtmlTableRows);
+
+    // This is the only syntax I could get to work for
+    // updating the values in the selected range.
+    if (!searchOnly) {
+      range.values = values;
+    }
 
     // The context.sync method sends
     // all queued commands to Excel for execution.
